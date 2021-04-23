9cea5648fcba6a7f03bd53bdd5bb6253bff12042
zatkiller <gheedylan@yahoo.com.sg>
2021-04-23 09:48:22 +0800

    Set up mine and other user video tabs

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { Grid, Typography, Paper, makeStyles } from "@material-ui/core";
+
+import { SocketContext } from "../SocketContext";
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: "550px",
+    [theme.breakpoints.down("xs")]: {
+      width: "300px",
+    },
+  },
+  gridContainer: {
+    justifyContent: "center",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+    },
+  },
+  paper: {
+    padding: "10px",
+    border: "2px solid black",
+    margin: "10px",
+  },
+}));
 
 const VideoPlayer = () => {
-  return <div>Video Player component</div>;
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.gridContainer}>
+      {/* Own video */}
+      <Paper className={classes.paper}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h5" gutterBottom>
+            NAME
+          </Typography>
+          <video
+            playsInline
+            muted
+            ref={null}
+            autoPlay
+            className={classes.video}
+          />
+        </Grid>
+      </Paper>
+      {/* User video */}
+      <Paper className={classes.paper}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h5" gutterBottom>
+            NAME
+          </Typography>
+          <video playsInline ref={null} autoPlay className={classes.video} />
+        </Grid>
+      </Paper>
+    </Grid>
+  );
 };
 
 export default VideoPlayer;
