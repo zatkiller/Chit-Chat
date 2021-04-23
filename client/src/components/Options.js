0a1169a6e85404c96fe995099c0689c24d016c49
zatkiller <gheedylan@yahoo.com.sg>
2021-04-23 09:57:25 +0800

    Add make a call

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -83,6 +83,40 @@ const Options = ({ children }) => {
                 </Button>
               </CopyToClipboard>
             </Grid>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">
+                Make a call
+              </Typography>
+              <TextField
+                label="ID to call"
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+                fullWidth
+              />
+              {callAccepted && !callEnded ? (
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  startIcon={<PhoneDisabled fontSize="large" />}
+                  fullWidth
+                  onClick={leaveCall}
+                  className={classes.margin}
+                >
+                  Hang Up
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<Phone fontSize="large" />}
+                  fullWidth
+                  onClick={() => callUser(idToCall)}
+                  className={classes.margin}
+                >
+                  Call
+                </Button>
+              )}
+            </Grid>
           </Grid>
         </form>
         {children}
