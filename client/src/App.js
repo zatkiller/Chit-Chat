64e75be60956e7a95bd72d3ca4ba55dc3a3e8804
zatkiller <gheedylan@yahoo.com.sg>
2021-04-23 09:18:10 +0800

    Add MUI Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import VideoPlayer from "./components/VideoPlayer";
+import Notifications from "./components/Notifications";
+import Options from "./components/Options";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
 
 const App = () => {
-  return <div>Video Chat App</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/VideoPlayer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoPlayer = () => {
+  return <div>Video Player component</div>;
+};
+
+export default VideoPlayer;
