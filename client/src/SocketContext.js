8cdfd815b3812ae99aa30bb2a284521cf5a0a036
zatkiller <gheedylan@yahoo.com.sg>
2021-04-23 11:17:57 +0800

    revert fix

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -1,33 +1,36 @@
+// React Context and  Socket logic all in this file, so that can be easily used by the components
 import React, { createContext, useState, useRef, useEffect } from "react";
 import { io } from "socket.io-client";
 import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-// const socket = io('http://localhost:5000');
-const socket = io("https://zoom-clone-webapp.herokuapp.com/");
+const socket = io("https://zoom-clone-webapp.herokuapp.com/"); //io("http://localhost:5000");
 
 const ContextProvider = ({ children }) => {
-  const [callAccepted, setCallAccepted] = useState(false);
-  const [callEnded, setCallEnded] = useState(false);
   const [stream, setStream] = useState();
-  const [name, setName] = useState("");
   const [call, setCall] = useState({});
   const [me, setMe] = useState("");
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
 
   const myVideo = useRef();
   const userVideo = useRef();
   const connectionRef = useRef();
 
   useEffect(() => {
+    // Get permission to use mic and camera from the user
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then((currentStream) => {
         setStream(currentStream);
 
+        // Populate video frame once loaded, hence need useRef
         myVideo.current.srcObject = currentStream;
       });
 
+    // Listen from the me action on the backend
     socket.on("me", (id) => setMe(id));
 
     socket.on("callUser", ({ from, name: callerName, signal }) => {
@@ -38,18 +41,26 @@ const ContextProvider = ({ children }) => {
   const answerCall = () => {
     setCallAccepted(true);
 
+    // Initiator - person who created the call
+    //
     const peer = new Peer({ initiator: false, trickle: false, stream });
 
+    // receive the signal, similar to socket on connection
+    // to - the person callng
     peer.on("signal", (data) => {
       socket.emit("answerCall", { signal: data, to: call.from });
     });
 
+    // start the stream on both videos
     peer.on("stream", (currentStream) => {
+      // Set the other person stream
       userVideo.current.srcObject = currentStream;
     });
 
+    //call.signal comes from socket, pass the state to peer
     peer.signal(call.signal);
 
+    // current connection = the perr connection
     connectionRef.current = peer;
   };
 
@@ -59,7 +70,7 @@ const ContextProvider = ({ children }) => {
     peer.on("signal", (data) => {
       socket.emit("callUser", {
         userToCall: id,
-        signalData: data,
+        signalData: data, //Pass the signal datra
         from: me,
         name,
       });
@@ -69,6 +80,7 @@ const ContextProvider = ({ children }) => {
       userVideo.current.srcObject = currentStream;
     });
 
+    // Accept the call
     socket.on("callAccepted", (signal) => {
       setCallAccepted(true);
 
@@ -83,9 +95,12 @@ const ContextProvider = ({ children }) => {
 
     connectionRef.current.destroy();
 
+    // reload the page to provide new id to the user
     window.location.reload();
   };
 
+  // All the variables inside the value will be globally accessbile to all components
+  // Context only has 1 thing of it, children
   return (
     <SocketContext.Provider
       value={{
