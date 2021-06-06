0d3bbabb5c5626b46ba03dabce139b53161f85a5
zatkiller <gheedylan@yahoo.com.sg>
2021-06-06 22:57:16 +0800

    Change app name

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ const App = () => {
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography variant="h2" align="center">
-          Video Chat
+          Chit Chat
         </Typography>
       </AppBar>
       <VideoPlayer />
